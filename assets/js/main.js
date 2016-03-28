77be4c1b00eab77b70a902245c90efed45583486
William Bradley <williambbradley@gmail.com>
2016-03-28 00:01:40 +0000

    push a super basic bit of logic and include the moment and moment-timezone libraries

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,2 +1,29 @@
+function updateDependencies(inputMoment) {
+    if (inputMoment) {
+        deltaElement.innerText = inputMoment.toString();
+        isoElement.innerText = inputMoment.toString();
+        localElement.innerText = inputMoment.toString();
+        epochElement.innerText = inputMoment.unix();
+    }
+}
+
+function inputUpdateHandler(event) {
+    /* moment is imported as a global */
+    var currentMoment = moment(event.target.value);
+    if (currentMoment.isValid()) {
+        queryElement.style.borderBottomColor = "white";
+        updateDependencies(currentMoment);
+    } else {
+        queryElement.style.borderBottomColor = "pink";
+    }
+}
+
 var queryElement = document.getElementById('js-query');
-queryElement.value = "sadf";
+queryElement.oninput = inputUpdateHandler;
+queryElement.onpropertychange = queryElement.oninput;
+
+var deltaElement = document.getElementById('js-delta');
+var isoElement = document.getElementById('js-iso');
+var epochElement = document.getElementById('js-epoch');
+var localElement = document.getElementById('js-local');
+
