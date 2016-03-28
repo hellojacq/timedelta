2c8bb60f61aaf2a623b7958a12bf2b9f9d266327
William Bradley <williambbradley@gmail.com>
2016-03-28 01:06:29 +0000

    put some validation styles on the input box

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,28 @@
+function hasClass(el, className) {
+    if (el.classList) {
+        return el.classList.contains(className);
+    } else {
+        return !!el.className.match(new RegExp('(\\s|^)' + className + '(\\s|$)'));
+    }
+}
+
+function addClass(el, className) {
+    if (el.classList) {
+        el.classList.add(className);
+    } else if (!hasClass(el, className)) {
+        el.className += " " + className;
+    }
+}
+
+function removeClass(el, className) {
+    if (el.classList) {
+        el.classList.remove(className);
+    } else if (hasClass(el, className)) {
+        var reg = new RegExp('(\\s|^)' + className + '(\\s|$)');
+        el.className=el.className.replace(reg, ' ');
+    }
+}
+
 function updateDependencies(inputMoment) {
     if (inputMoment) {
         deltaElement.innerText = inputMoment.toString();
@@ -9,12 +34,21 @@ function updateDependencies(inputMoment) {
 
 function inputUpdateHandler(event) {
     /* moment is imported as a global */
-    var currentMoment = moment(event.target.value);
-    if (currentMoment.isValid()) {
-        queryElement.style.borderBottomColor = "white";
-        updateDependencies(currentMoment);
+    removeClass(queryElement, "valid-input");
+    removeClass(queryElement, "invalid-input");
+    removeClass(queryElement, "no-input");
+
+    var input = event.target.value;
+    if (input) {
+        var currentMoment = moment(event.target.value);
+        if (currentMoment.isValid()) {
+            addClass(queryElement, "valid-input");
+            updateDependencies(currentMoment);
+        } else {
+            addClass(queryElement, "invalid-input");
+        }
     } else {
-        queryElement.style.borderBottomColor = "pink";
+        addClass(queryElement, "no-input");
     }
 }
 
@@ -27,3 +61,6 @@ var isoElement = document.getElementById('js-utc');
 var epochElement = document.getElementById('js-epoch');
 var localElement = document.getElementById('js-local');
 
+/* begin by running the inputUpdateHandler on the input to ensure
+ * any styles that we might want to apply are applied */
+inputUpdateHandler({target: queryElement});
